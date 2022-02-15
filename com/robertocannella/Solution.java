ed4dc0399d6b0200c58cb6bbc97f705856432e4e
robertocannella <robertocannella@gmail.com>
2022-02-14 19:17:45 -0500

    Simplified Solution Class leetcode - firstMissingPositive() passed

--- com/robertocannella/Solution.java
+++ com/robertocannella/Solution.java
@@ -6,35 +6,17 @@ import java.util.*;
 class Solution {
         public static int firstMissingPositive(int[] nums) {
 
+            Arrays.sort(nums);
+            int min=1;
 
-            int currentItem = 0;
-            int nextItem = 0;
-            PriorityQueue<Integer> pq = new PriorityQueue<>();
-
-            for (int i = 0; i <nums.length -1 ; i++) {
-                if (nums[i] > 0) {
-                        if (!pq.contains(nums[i]))
-                            pq.add(nums[i]);
-                }
-            }
-
-            if (pq.peek() == null)
-                return 1;
-            if (pq.peek() > 1)
-                return 1;
-
-            while(!pq.isEmpty()) {
-                currentItem = pq.poll();
-                if (pq.peek() == null)
-                    break;
-                nextItem = pq.peek();
-               if (nextItem - currentItem == 1 ){
-                   continue;
-               }
-               break;
-            }
-            return currentItem  + 1;
+            for (int num : nums)
+                    if (num == min)
+                        min++;
+            return min;
         }
+
+
+
     }
     class Client {
 
@@ -47,8 +29,8 @@ class Solution {
             arr[i] = rd.nextInt(); // storing random integers in an array
             //System.out.println(arr[i]); // printing each array element
         }
-        int [] array = {0,2,2,1,1};
-        // int[] array = {-1,-8,3 ,4, 5,6,7,8,9,10,15};
+        //int [] array = {0,2,2,1,1};
+        int[] array = {-1,-8,3 ,4, 5,6,7,8,9,10,15};
         //int[] array = {3,6,3,8,431,0,-3,3,10,3,90,89,-45,-90};
         int test = Solution.firstMissingPositive(array);
         System.out.println(test);
